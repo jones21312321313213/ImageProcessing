4ab799104b68e47c3d061a927d7384fd7ce0e473
jones21312321313213 <skinnyjones212121@gmail.com>
2025-09-23 20:56:17 +0800

    added webcam using aforge lib

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -31,11 +31,17 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.iDKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.changeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greyscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.colorInversionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sepiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -48,7 +54,7 @@
             this.changeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(994, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -60,17 +66,29 @@
             // 
             // changeToolStripMenuItem
             // 
+            this.changeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.greyscaleToolStripMenuItem,
+            this.colorInversionToolStripMenuItem,
+            this.sepiaToolStripMenuItem});
             this.changeToolStripMenuItem.Name = "changeToolStripMenuItem";
             this.changeToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.changeToolStripMenuItem.Text = "Change";
             // 
+            // greyscaleToolStripMenuItem
+            // 
+            this.greyscaleToolStripMenuItem.Name = "greyscaleToolStripMenuItem";
+            this.greyscaleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.greyscaleToolStripMenuItem.Text = "Greyscale";
+            this.greyscaleToolStripMenuItem.Click += new System.EventHandler(this.greyscaleToolStripMenuItem_Click_1);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(82, 101);
+            this.pictureBox1.Location = new System.Drawing.Point(41, 101);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(249, 230);
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
@@ -84,7 +102,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(514, 39);
+            this.button2.Location = new System.Drawing.Point(673, 39);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(165, 40);
             this.button2.TabIndex = 3;
@@ -94,17 +112,63 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(453, 101);
+            this.pictureBox2.Location = new System.Drawing.Point(535, 101);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(249, 230);
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(85, 497);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(165, 37);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(673, 497);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(433, 58);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(121, 21);
+            this.cboCamera.TabIndex = 7;
+            // 
+            // colorInversionToolStripMenuItem
+            // 
+            this.colorInversionToolStripMenuItem.Name = "colorInversionToolStripMenuItem";
+            this.colorInversionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colorInversionToolStripMenuItem.Text = "Color inversion";
+            this.colorInversionToolStripMenuItem.Click += new System.EventHandler(this.colorInversionToolStripMenuItem_Click);
+            // 
+            // sepiaToolStripMenuItem
+            // 
+            this.sepiaToolStripMenuItem.Name = "sepiaToolStripMenuItem";
+            this.sepiaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sepiaToolStripMenuItem.Text = "Sepia";
+            this.sepiaToolStripMenuItem.Click += new System.EventHandler(this.sepiaToolStripMenuItem_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(994, 559);
+            this.Controls.Add(this.cboCamera);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -113,6 +177,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -132,5 +197,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.ToolStripMenuItem greyscaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorInversionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sepiaToolStripMenuItem;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Tracing;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using WebCamLib;
 namespace ImageProcessing
 {
     public partial class Form3 : Form
     {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        Device myDevice;
         public Form3()
         {
             InitializeComponent();
@@ -30,5 +36,184 @@ namespace ImageProcessing
             f2.Show();
             this.Hide();
         }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach(FilterInfo filterInfo in filterInfoCollection)
+            {
+                cboCamera.Items.Add(filterInfo.Name);
+            }
+            cboCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Device[] devices = DeviceManager.GetAllDevices();
+
+            //if (devices.Length > 0)
+            //{
+            //    myDevice = devices[0];
+
+            //    myDevice.ShowWindow(pictureBox1);
+            //}
+
+            //foreach (var d in devices)
+            //{
+            //    MessageBox.Show(d.Name); 
+            //}
+
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+
+        }
+        bool grayscaleOn = false;
+        bool colorInversionOn = false;
+        bool sepiaOn = false;
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame = new Bitmap(eventArgs.Frame);
+            
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+            if (grayscaleOn)
+            {
+                greyScale2(sender, frame);
+               
+            }
+
+            if (colorInversionOn)
+            {
+                colorInversion(sender, frame);
+                
+            }
+
+            if (sepiaOn)
+            {
+                sepia(sender, frame);
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //if (myDevice != null) { 
+            //    myDevice.Stop();
+            //}
+           
+            if(videoCaptureDevice.IsRunning == true)
+            {
+                videoCaptureDevice.Stop();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void greyscaleToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            colorInversionOn = false;
+            sepiaOn = false;
+            grayscaleOn = !grayscaleOn;
+        }
+
+        private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            grayscaleOn = false;
+            sepiaOn = false;
+            colorInversionOn = !colorInversionOn;
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            grayscaleOn = false;
+            colorInversionOn = false;
+            sepiaOn = !sepiaOn;
+        }
+
+        private void greyScale2(object sender,Bitmap frame)
+        {
+            
+            Bitmap grayFrame = new Bitmap(frame.Width, frame.Height);
+
+            for (int x = 0; x < frame.Width; x++)
+            {
+                for (int y = 0; y < frame.Height; y++)
+                {
+                    Color p = frame.GetPixel(x, y);
+                    int gray = (p.R + p.G + p.B) / 3;
+                    grayFrame.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                }
+            }
+
+            pictureBox2.Image = grayFrame;
+
+        }
+
+        private void colorInversion(object sender, Bitmap frame)
+        {
+
+            Bitmap newPic = new Bitmap(frame.Width, frame.Height);
+
+            for (int i = 0; i < frame.Width; i++)
+            {
+                for (int j = 0; j < frame.Height; j++)
+                {
+                    Color p = frame.GetPixel(i, j);
+
+                    int r = 255 - p.R;
+                    int gg = 255 - p.G;
+                    int b = 255 - p.B;
+
+                    Color g = Color.FromArgb(r, gg, b);
+
+                    newPic.SetPixel(i, j, g);
+                }
+            }
+
+            pictureBox2.Image = newPic;
+
+        }
+        private void sepia(object sender, Bitmap frame)
+        {
+
+            Bitmap newPic = new Bitmap(frame.Width, frame.Height);
+
+            for (int i = 0; i < frame.Width; i++)
+            {
+                for (int j = 0; j < frame.Height; j++)
+                {
+                    Color p = frame.GetPixel(i, j);
+
+                    int r = (int)(p.R * 0.393 + p.G * 0.769 + p.B * 0.189);
+                    int gg = (int)(p.R * 0.349 + p.G * 0.686 + p.B * 0.168);
+                    int b = (int)(p.R * 0.272 + p.G * 0.534 + p.B * 0.131);
+
+                    // make sure none of these will go beyond 255
+                    r = Math.Min(255, r);
+                    gg = Math.Min(255, gg);
+                    b = Math.Min(255, b);
+
+                    Color s = Color.FromArgb(r, gg, b);
+
+                    newPic.SetPixel(i, j, s);
+                }
+            }
+
+            pictureBox2.Image = newPic;
+
+        }
+
     }
 }
